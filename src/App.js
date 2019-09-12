344ce12c28c250542d74f16e5739b621d9ec6660
Fira Cloud Tech <firacloudtech@gmail.com>
2019-09-12 08:04:36 +0800

    Sept 12

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Homepage from './components/HomePage/Homepage';
+
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      <Homepage></Homepage>
+    </div>
+    )
+  }
+}
